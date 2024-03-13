845dd929ffe6a0944ac28f16fdfe11ef777943a0
Patrick Tamunjoh <38542007+TheMoonrise@users.noreply.github.com>
2024-03-13 17:33:10 +0100

    feat: define transitive network function attribute

---
+++ Runtime/Functions/TransitiveNetworkFunction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MoonriseGames.CloudsAhoyConnect.Enums;
+
+namespace MoonriseGames.CloudsAhoyConnect.Functions
+{
+    /// <summary>
+    /// Attribute for marking functions to be transitively synced over the network.
+    /// A function with this attribute should only be called from a <see cref="NetworkFunction"/> or other <see cref="TransitiveNetworkFunction"/>.
+    /// Any function that modifies the shared network state should be of one of these two types.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public class TransitiveNetworkFunction : Attribute { }
+}
